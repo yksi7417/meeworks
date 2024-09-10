5c8fd3d9fe730d740c9dc21ab9824c4da4a54288
Anthony Si <yatkasi@gmail.com>
2024-09-09 20:33:32 -0400

    add index plage

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ async fn hello_world() -> &'static str {
     "Hello, world 🀅!"
 }
 
+async fn index_page() -> &'static str {
+    "Hello, world 🀅!"
+}
+
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new().route("/", get(index_page));
     let router = Router::new().route("/hello", get(hello_world));
-
     Ok(router.into())
 }
